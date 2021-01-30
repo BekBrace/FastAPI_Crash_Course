917174b6cd723f8a7f01f4c47188b67ddb08ca57
Bek Brace <60483846+BekBrace@users.noreply.github.com>
2021-01-30 23:10:27 +0100

    Update app.py
    Update app.py code and some comments

--- FASTAPI/app/app.py
+++ FASTAPI/app/app.py
@@ -1,22 +1,24 @@
+# Importing the FastApi class
 from fastapi import FastAPI
 
+# Creating an app object
 app = FastAPI()
 
 # Default route
 
-
+# A minimal app to demonstrate the get request 
 @app.get("/", tags=['root'])
 async def root() -> dict:
     return {"Ping": "Pong"}
 
 
-# Get [Read] Todo Route
+# GET -- > Read Todo 
 @app.get("/todo", tags=['Todos'])
 async def get_todos() -> dict:
     return {"Data": todos}
 
 
-# Post [Create] Todo Route
+# Post -- > Create Todo
 @app.post("/todo", tags=["Todos"])
 async def add_todo(todo: dict) -> dict:
     todos.append(todo)
@@ -25,11 +27,11 @@ async def add_todo(todo: dict) -> dict:
     }
 
 
-# PUT  [Update] Todo Route
+# PUT  -- > Update Todo
 @app.put("/todo/{id}", tags=["Todos"])
 async def update_todo(id: int, body: dict) -> dict:
     for todo in todos:
-        if (int(todo["ID"])) == id:
+        if (int(todo["id"])) == id:
             todo["Activity"] = body["Activity"]
             return {
                 "data": f"Todo with id {id} has been updated"
@@ -39,11 +41,11 @@ async def update_todo(id: int, body: dict) -> dict:
     }
 
 
-# Delete Todo Route
+# DELETE --> Delete Todo 
 @app.delete("/todo/{id}", tags=["Todos"])
 async def delete_todo(id: int) -> dict:
     for todo in todos:
-        if int(todo["ID"]) == id:
+        if int(todo["id"]) == id:
             todos.remove(todo)
             return{
                 "data": f"Todo with id {id} has been deleted!"
@@ -56,11 +58,11 @@ async def delete_todo(id: int) -> dict:
 
 todos = [
     {
-        "ID": "1",
+        "id": "1",
         "Activity": "Jogging for 2 hours at 7:00 AM."
     },
     {
-        "ID": "2",
+        "id": "2",
         "Activity": "Writing 3 pages of my new book at 2:00 PM."
     }
 ]
